80e6be09f62818fc9fa9cc45b5a549a86b675a48
Sama Mansour <samamansour306@gmail.com>
2023-08-28 11:38:13 +0300

    Fix room slider

--- src/components/RoomsSlider/RoomsSlider.js
+++ src/components/RoomsSlider/RoomsSlider.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Text, Flex, Image } from "@chakra-ui/react";
+import { Box, Text, Flex, Image, VStack } from "@chakra-ui/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -15,40 +15,51 @@ const RoomsSlider = () => {
   };
 
   return (
-    <Flex position="relative" maxWidth="100%" overflow="hidden" height="400px">
-      {/* Left - Discover our collection */}
+    <Box position="relative" maxWidth="100%" overflow="hidden" height="400px">
+      {/* Slider */}
+      
+
+      {/* Discover Collection Box on the Left */}
       <Flex
-        width="50%"
+        position="absolute"
+        top="20%"
+        left="5%"
         flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        bg="rgba(0, 0, 0, 0.7)"
-        color="white"
+        width="30%"
         p={4}
+        bg="rgba(255, 255, 255, 0.8)"
+        boxShadow="2px 2px 15px rgba(0, 0, 0, 0.5)" // 3D shadow effect
+        border="2px solid #FFF3E3" // Sharp edge effect
+        bgColor="#FFF3E3"
       >
-        <Text fontSize="2xl" fontWeight="bold">
-          Discover our collection
-        </Text>
-        <Text fontSize="lg">50+ Beautiful rooms inspiration</Text>
+        <VStack spacing={4} textAlign="left">
+          <Text fontSize="2xl" fontWeight="bold" color="#B88E2F">
+            Discover our rooms
+          </Text>
+          <Text>50+ Beautiful rooms inspiration</Text>
+        </VStack>
       </Flex>
 
-      {/* Right - Slider */}
-      <Box width="50%">
-        <Slider {...settings}>
-          <Image
-            src="../../assets/images/room1.png"
-            alt="Room 1"
-            width="100%"
-          />
-          <Image
-            src="../../assets/images/room2.png"
-            alt="Room 2"
-            width="100%"
-          />
-          {/* Add more images as needed */}
-        </Slider>
-      </Box>
-    </Flex>
+      {/* Floating Images in a Zigzag Pattern */}
+   
+      <Image
+        src="../../assets/images/room1.png"
+        alt="Floating Image 1"
+        position="absolute"
+        top="10%"
+        right="20%"
+        width="10%"
+      />
+      <Image
+        src="../../assets/images/room2.png"
+        alt="Floating Image 2"
+        position="absolute"
+        top="30%"
+        right="10%"
+        width="10%"
+      />
+    
+    </Box>
   );
 };
 
